2125b5a6c24a9092a9339dfa3d9f23261ff8d0fc
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-10-15 15:15:26 +0600

    data called

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@Components/Navbar";
+import data from "@Data/data.json";
 import AppLayout from "@layout/layout";
 import { useState } from "react";
 require("./index.less");
 
 const index = () => {
   const [visible, setVisible] = useState(false);
+
+  console.log(data);
 
   return (
     <AppLayout>
