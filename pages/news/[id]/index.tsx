c3ac9ba04e2ebd456f2d83b710b63e0fe4b8d95f
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-10-15 14:57:51 +0600

    fav icon page pAlo image

--- pages/news/[id]/index.tsx
+++ pages/news/[id]/index.tsx
@@ -1,14 +1,7 @@
-import FastDelivery from "@Components/FastDelivery";
-import Footer from "@Components/Footer";
-import Loader from "@Components/Loader";
 import Navbar from "@Components/Navbar";
-import PopularProduct from "@Components/PopularProduct";
-import ProductById from "@Components/ProductById";
-import Topbar from "@Components/Topbar";
 import AppLayout from "@layout/layout";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import DataActions from "redux/actions/DataActions";
 require("./index.less");
 
 const index = () => {
@@ -21,59 +14,14 @@ const index = () => {
   const [pageLoad, setPageLoad]: any = useState(true);
   const [rng, setRng]: any = useState(Math.random);
 
-  useEffect(() => {
-    if (router.isReady) {
-      getData(id);
-      getData2();
-    } else {
-      return;
-    }
-  }, [router.isReady, rng]);
-
-  const getData = (id: any) => {
-    setPageLoad(true);
-
-    DataActions.getOneData(
-      id,
-      (success: any) => {
-        console.log("success=>", success);
-        setPageLoad(false);
-        setOneData(success);
-      },
-      (error: any) => {
-        console.log("error=>", error);
-      }
-    );
-  };
-
-  const getData2 = () => {
-    setLoading(true);
-
-    DataActions.getData(
-      3,
-      (success: any) => {
-        console.log("success=>", success);
-        setLoading(false);
-        setData(success);
-      },
-      (error: any) => {
-        console.log("error=>", error);
-      }
-    );
-  };
 
   if (pageLoad || loading) {
-    return <Loader></Loader>;
+    // return <Loader></Loader>;
   }
 
   return (
     <AppLayout>
-      <Topbar></Topbar>
       <Navbar></Navbar>
-      <ProductById data={oneData} data2={data} setRng={setRng} getData={getData}></ProductById>
-      <FastDelivery></FastDelivery>
-      <PopularProduct></PopularProduct>
-      <Footer></Footer>
     </AppLayout>
   );
 };
